9bf94ae1109b2b166607e0b57f895aa0a22b7e32
maxik <maximoalejandro.kirichuk@alumnos.uai.edu.ar>
2025-03-28 16:05:03 -0300

    readme terminado

--- src/hooks/useDataBase.js
+++ src/hooks/useDataBase.js
@@ -1,17 +1,16 @@
 import * as SQlite from 'expo-sqlite';
 
 export const useDataBase = () => {
-    
+    // Abrir la base de datos
     const openDataBase = async () => {
         const db = await SQlite.openDatabaseAsync("sessions.db");
         return db;
     }
+    //Inicializar la base de datos (Crear la tabla)
     const initDB = async () => {
         const db = await openDataBase();
-        console.log(db)
         const sql = `CREATE TABLE IF NOT EXISTS sessions (localId TEXT PRIMARY KEY NOT NULL, email TEXT NOT NULL, token TEXT NOT NULL);`;
         const res = await db.execAsync(sql)
-        console.log(res)
         return res;
       };
 
@@ -20,25 +19,22 @@ export const useDataBase = () => {
         const sql = `INSERT INTO sessions (localId, email, token) VALUES (?, ?, ?);`;
         const args = [localId, email, token];
         const res = await db.runAsync(sql, args);
-        console.log(res);
         return res;
       };
-
+      //Obtener una sesión
     const getSession = async () => {
         const db = await openDataBase();
         const sql = `SELECT * FROM sessions`
         const firstRow = await db.getFirstAsync(sql);
-        console.log('firstRow', firstRow);
         return firstRow;
     }
-
+    //Borrar todas las sesiones (Cerrar sesión)
     const truncateSessionTable = async () => {
         const db = await openDataBase();
         //borra todo
         const sql = `DELETE FROM sessions`
         const args = [];
         const res = await db.execAsync(sql);
-        console.log(res);
         return res
     }
 
